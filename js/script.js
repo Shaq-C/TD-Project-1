4cca07132d5553c520d86e9796a6c2f82dd14839
Shaq-C <shaqcahill@gmail.com>
2021-03-01 21:01:09 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,20 +10,61 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [  
+  {quote:'If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.', 
+   source:'Steve Jobs',
+   citation:'Motivating thoughts of Steve Jobs', 
+   year: 2009},
+  {quote: "Mama always said life was like a box of chocolates. You never know what you're gonna get", 
+   source:'Tom Hanks',
+   citation:'Forrest Gump', 
+   year: 1994},
+  {quote:'Carpe diem. Seize the day, boys. Make your lives extraordinary.', 
+   source:'Robin Williams',
+   citation:'Dead Poets Society', 
+   year: 1989},
+  {quote:'Who controls the past controls the future. Who controls the present controls the past.', 
+   source:'George Orwell',
+   citation:'Nineteen Eighty-Four', 
+   year: 1949},
+  {quote:'There is some good in this world, and it’s worth fighting for.', 
+   source:'J.R.R. Tolkien',
+   citation:'The Two Towers', 
+   year: 1954}
 
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote () {
+  let randomQuote = Math.floor(Math.random() * quotes.length);
+  let qt = quotes[randomQuote];
+  return qt;
+}
 
 
 /***
  * `printQuote` function
 ***/
-
-
+function printQuote(){
+  let j = getRandomQuote();
+  let html=''
+  html = 
+    `<p class="quote">${j.quote}` +
+    `<p class="source">${j.source}`;
+      if ( j.citation == true ) {
+      html + `<span class="citation">${j.citation}</span>`}
+      if ( typeof j.year == Number){ 
+      html +`<span class="year">${j.year}</span>`}
+    `</p>`
+    
+        
+  document.getElementById("quote-box").innerHTML = html;
+  
+}
+console.log(printQuote());
 
 /***
  * click event listener for the print quote button
